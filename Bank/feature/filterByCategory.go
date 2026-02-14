46a35cb1e2bb6cbf425e71220b92cd3b248ae252
surush3005 <surush3005@gmail.com>
2026-02-14 15:52:16 +0500

    changes

--- Bank/feature/filterByCategory.go
+++ Bank/feature/filterByCategory.go
@@ -1,16 +1,15 @@
 package filtrByCategory
 
 import (
- "github.com/surush3005/Bank.git/pkg/bank/types"
+	"github.com/surush3005/Bank.git/pkg/bank/types"
 )
-	
 
-
-func FiltrByCategory(payments []types.Payment, category types.Category) []types.Payment {
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
 	var filetred []types.Payment
 	for _, payment := range payments {
 		if payment.Category == category {
 			filetred = append(filetred, payment)
 		}
 	}
+	return filetred
 }
